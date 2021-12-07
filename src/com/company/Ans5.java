7d96d3204c673eb4b8a32c713fc96aee49ba2e78
Shardhay v <shardhayvatshyayan7@gmail.com>
2021-12-07 11:04:34 +0545

    Ans5 added of assignment1

---
+++ src/com/company/Ans5.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Ans5 {
+    public static void main(String[] args) {
+        int rows=4;
+        for (int i= 0; i<= rows; i++)
+        {
+            for (int k=0; k<=rows; k++)
+            {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
